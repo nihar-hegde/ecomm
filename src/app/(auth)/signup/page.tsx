4efa863d135e908d3acfcb1170ef728e36553679
Nihar Hegde <niharhegde163@gmail.com>
2023-08-17 16:10:05 +0530

    fixing minor bug while routing to signin and signup

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-red">Hello world </div>;
+};
+
+export default page;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "./ui/button";
+import { LogIn, ShoppingCart } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="bg-zinc-200 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
-      <div className="container">Login</div>
+      <div className="container flex items-center justify-between">
+        <Link href={"/"}>
+          <ShoppingCart />
+        </Link>
+        <Link href={"/signin"} className={buttonVariants()}>
+          <LogIn className="mr-2 h-4 w-4" /> Sign In
+        </Link>
+      </div>
     </nav>
   );
 };
