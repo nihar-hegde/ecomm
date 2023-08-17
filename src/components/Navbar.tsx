922b57739d3d5619608645ac625eb59ed7e0d88b
Nihar Hegde <niharhegde163@gmail.com>
2023-08-17 15:38:19 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-zinc-200 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div className="container">Login</div>
+    </nav>
+  );
+};
+
+export default Navbar;
