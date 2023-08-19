c45b85a42c34d26f53ba16015fee59fe93995e7a
Nihar Hegde <niharhegde163@gmail.com>
2023-08-19 17:12:13 +0530

    logout button successfully working

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/signin");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <hr />
+      <Button onClick={logout} variant="destructive" className="mt-2">
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default ProfilePage;
